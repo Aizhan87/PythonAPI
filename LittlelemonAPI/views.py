e7018a5aff93e677002b26802b71d8d74d3c45e4
Aizhan87 <moonsoul87@hotmail.com>
2023-09-12 18:37:49 -0600

    added post method to menu items

--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import generics 
+from rest_framework import status 
 from .models import MenuItem
 from .models import Category 
 from .serializers import MenuItemSerializer
 from .serializers import CategorySerializer
 from django.shortcuts import get_object_or_404
 
-@api_view()
+@api_view(['GET', 'POST'])
 def menu_items(request):
-    items = MenuItem.objects.select_related('category').all()
-    serialized_item = MenuItemSerializer(items, many=True, context={'request': request})
-    return Response(serialized_item.data)
+    if request.method == 'GET':
+        items = MenuItem.objects.select_related('category').all()
+        serialized_item = MenuItemSerializer(items, many=True, context={'request': request})
+        return Response(serialized_item.data)
+    if request.method == 'POST':
+        serialized_item = MenuItemSerializer(data=request.data)
+        serialized_item.is_valid(raise_exception=True)
+        serialized_item.save()
+        return Response(serialized_item.data, status.HTTP_201_CREATED)
 
 @api_view()
 def single_item(request, id):
