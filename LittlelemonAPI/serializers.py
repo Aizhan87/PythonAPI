9ab95240f5898f137933efe8d446939b07dd6954
Aizhan87 <moonsoul87@hotmail.com>
2023-09-13 13:27:39 -0600

    added API filter in views in LittlelemonAPI

--- LittlelemonAPI/serializers.py
+++ LittlelemonAPI/serializers.py
@@ -8,17 +8,21 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'slug', 'title']
-
+        
+        
 class MenuItemSerializer(serializers.ModelSerializer):
-    stock = serializers.IntegerField(source='inventory')
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
-    category = CategorySerializer(read_only=True)
     category_id = serializers.IntegerField(write_only=True)
+    category = CategorySerializer(read_only=True)
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category', 'category_id']
-        
-    def calculate_tax(self, product: MenuItem):
+        fields = ['id','title','price','stock', 'price_after_tax','category','category_id']
+        extra_kwargs = {
+            'price': {'min_value': 2},
+            'stock':{'source':'inventory', 'min_value': 0}
+        }
+
+    def calculate_tax(self, product:MenuItem):
         return product.price * Decimal(1.1)
     
     
--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -12,6 +12,25 @@ from django.shortcuts import get_object_or_404
 def menu_items(request):
     if request.method == 'GET':
         items = MenuItem.objects.select_related('category').all()
+        category_name = request.query_params.get('category')
+        to_price = request.query_params.get('to_price')
+        search = request.query_params.get('search')
+        ordering = request.query_params.get('ordering')
+        if category_name: 
+            items = items.filter(category__title=category_name)
+        if to_price:
+            items = items.filter(price__lte=to_price)
+            # http://127.0.0.1:8000/apis/menu-items?to_price=7.50&category=Dessert
+        if search:
+            items = items.filter(title__icontains=search)
+            # http://127.0.0.1:8000/apis/menu-items?search=Chocolate
+        if ordering:
+            # items = items.order_by(ordering) -> order by 1 item
+            # http://127.0.0.1:8000/apis/menu-items?ordering=price  -> ascending order
+            # http://127.0.0.1:8000/apis/menu-items?ordering=-price -> descending order
+            ordering_fields = ordering.split(",")
+            items = items.order_by(*ordering_fields)
+            # http://127.0.0.1:8000/apis/menu-items?ordering=title,price
         serialized_item = MenuItemSerializer(items, many=True, context={'request': request})
         return Response(serialized_item.data)
     if request.method == 'POST':
@@ -19,6 +38,9 @@ def menu_items(request):
         serialized_item.is_valid(raise_exception=True)
         serialized_item.save()
         return Response(serialized_item.data, status.HTTP_201_CREATED)
+    
+# limit = request.GET.get(‘limit’)
+# MenuItem.objects.raw('SELECT * FROM LittleLemonAPI_menuitem LIMIT %s', [limit]) 
 
 @api_view()
 def single_item(request, id):
